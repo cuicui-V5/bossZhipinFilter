f46e847a200570f015872bf934e577073efe1d09
xrswyea <cjy1943222010@qq.com>
2023-09-11 14:48:31 +0800

    修复切换url不更新的问题

--- index.js
+++ index.js
@@ -41,7 +41,7 @@
         if (currentURL !== previousURL) {
             // URL发生了变化
             console.log("URL发生了变化:", currentURL);
-            init();
+            setTimeout(init, 2000);
             previousURL = currentURL; // 更新previousURL
         }
     };
